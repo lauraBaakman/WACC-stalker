040147be43b2f0599f756ff0ec727eba4eda6429
Laura Baakman <laurabaakman@gmail.com>
2014-09-18 22:29:13 +0200

    Completely fixed input validation on the search directive partial.

--- www/frontend/app/scripts/directives/search.js
+++ www/frontend/app/scripts/directives/search.js
@@ -12,22 +12,25 @@ define(['./module'], function (directives) {
                     $(this).css('margin-top', $(this).prev().height()-$(this).height());
                 });
 
+                // Apply bootstrap classes based on angular error classes
                 $("input").change(function(event) {
-                    console.log($(this));
-                    if ($(this).hasClass('ng-dirty ng-invalid')) {
-                        $(this).parent().addClass('debug');
+                    // Avoid validating empty inputs
+                    if(!$(this).val().length){
+                        $(this).parent().removeClass('has-error').removeClass('has-success');
+                    }
+
+                    // No need to check for ng-dirty since this is activated on change
+                    if ($(this).hasClass('ng-invalid')) {
+                        $(this).parent().addClass('has-error');
+                    } else if ($(this).hasClass('ng-valid')) {
+                        $(this).parent().addClass('has-success');
                     }
                 });
 
+                // Remove error class on focus
                 $("input").focus(function(event) {
-                    $(this).parent().removeClass('debug');
+                    $(this).parent().removeClass('has-error');
                 });
-                    
-
-
-    			this.sayMessage = function() {
-    				return this.message;
-    			};
 
                 this.searchSubmit = function(){
                     alert("Submitting the form!");
