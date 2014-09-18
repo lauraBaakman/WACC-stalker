3d70e8a1d930570608cc07f0d820f8177d678d39
Laura Baakman <laurabaakman@gmail.com>
2014-09-18 21:56:16 +0200

    Used jquery to convert angular error classes to bootstrap.

--- www/frontend/app/scripts/directives/search.js
+++ www/frontend/app/scripts/directives/search.js
@@ -7,9 +7,23 @@ define(['./module'], function (directives) {
     		controller: function() {
     			this.search = {};
 
+                // Align the submit button to the input fields
                 $('.pull-down').each(function() {
                     $(this).css('margin-top', $(this).prev().height()-$(this).height());
                 });
+
+                $("input").change(function(event) {
+                    console.log($(this));
+                    if ($(this).hasClass('ng-dirty ng-invalid')) {
+                        $(this).parent().addClass('debug');
+                    }
+                });
+
+                $("input").focus(function(event) {
+                    $(this).parent().removeClass('debug');
+                });
+                    
+
 
     			this.sayMessage = function() {
     				return this.message;
