4a2a003360d5e118dc04cb4ee1e44c10af511117
Laura Baakman <laurabaakman@gmail.com>
2014-09-25 12:34:48 +0200

    Small change sin whitespace.

--- www/frontend/app/scripts/directives/header.js
+++ www/frontend/app/scripts/directives/header.js
@@ -7,8 +7,7 @@ define(['./module'], function(directives) {
                 restrict: 'E',
                 templateUrl: '../views/header.html',
                 controller: function($scope) {
-                    // This shouldn't be a controller but couldnt't get it work with ng-include.
-                },
+                }
             };
         }
     ]);
