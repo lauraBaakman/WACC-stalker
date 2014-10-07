76c3f9c8a17ffdfe786be19603ed994d4bbf0692
Laura Baakman <laurabaakman@gmail.com>
2014-10-07 21:33:11 +0200

    Extracted the base url from the url in the api service.

--- www/frontend/app/scripts/services/api-service.js
+++ www/frontend/app/scripts/services/api-service.js
@@ -4,10 +4,11 @@ define(['./module'], function(services){
 	services.service('apiService', ['$http', 
 		function($http) {
 			return {
-				//todo url eruit trekken naar base url
+				baseurl: 'http://127.0.0.1:5000/',
+				
 				getAllSearches: function() {
 					console.log('getAllSearches in service is called.');
-					return $http.get('http://127.0.0.1:5000/search').
+					return $http.get(this.baseurl + 'search').
 						then(function(result){
 							return result.data;
 						});
