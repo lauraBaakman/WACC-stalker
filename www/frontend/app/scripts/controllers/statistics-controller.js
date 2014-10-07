2a01717128f5bced911ad91bfa6054810fb51706
Laura Baakman <laurabaakman@gmail.com>
2014-10-07 23:17:01 +0200

    Statistics page now also shows the stalkers and victims.

--- www/frontend/app/scripts/controllers/statistics-controller.js
+++ www/frontend/app/scripts/controllers/statistics-controller.js
@@ -18,6 +18,35 @@ define(['./module'], function(controllers) {
 					}
 				);
 			};
+
+			this.getAllStalkers = function() {
+				console.log('in get all stalkers in de controller')
+				$scope.stalkers = [];
+				$scope.error = {};
+				apiService.getAllStalkers().then(
+					function(stalkers){
+						console.log('Succes: ' + stalkers)
+						$scope.stalkers = stalkers;
+					},
+					function(error){
+						console.log('Error!')
+						$scope.error = 'The requenst for all stalkers failed.';
+					}
+				);
+			};
+
+			this.getAllVictims = function() {
+				$scope.victims = [];
+				$scope.error = {};
+				apiService.getAllVictims().then(
+					function(victims){
+						$scope.victims = victims;
+					},
+					function(error){
+						$scope.error = 'The requenst for all victims failed.';
+					}
+				);
+			};			
 		}
 	]);
 });
--- www/frontend/app/scripts/services/api-service.js
+++ www/frontend/app/scripts/services/api-service.js
@@ -7,12 +7,26 @@ define(['./module'], function(services){
 				baseurl: 'http://127.0.0.1:5000/',
 				
 				getAllSearches: function() {
-					console.log('getAllSearches in service is called.');
 					return $http.get(this.baseurl + 'search').
 						then(function(result){
 							return result.data;
 						});
-				}
+				},
+
+				getAllStalkers: function() {
+					console.log('in get all stalkers in de service')
+					return $http.get(this.baseurl + 'stalker').
+						then(function(result){
+							return result.data;
+						});					
+				},
+
+				getAllVictims: function() {
+					return $http.get(this.baseurl + 'victim').
+						then(function(result){
+							return result.data;
+						});					
+				}				
 			};
 		}
 	]);
