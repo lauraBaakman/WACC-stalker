4988d7fbcc603af3f23bd7eae46d3e40596d72be
Laura Baakman <laurabaakman@gmail.com>
2014-10-21 14:40:37 +0200

    FF able to pass a string to a directive.

--- www/frontend/app/scripts/directives/statistics-frequency.js
+++ www/frontend/app/scripts/directives/statistics-frequency.js
@@ -4,9 +4,19 @@ define(['./module'], function(directives) {
         function(searchService, $scope) {
             return {
                 restrict: 'E',
+                scope: {
+                    ratingValue: '='
+                },
                 templateUrl: '../views/statistics-frequency.html',
+                // link: function(scope, elem, attrs) {
+                //     scope.stars = [];
+                //     console.log(scope.ratingValue);
+                //     for (var i = 0; i < scope.ratingValue; i++) {
+                //         scope.stars.push({});
+                //     }
+                // },
                 controller: function($scope) {
-                    console.log('stat-freq-controller');
+                    console.log('Rating value in de controller:' + $scope.ratingValue);
                 },
                 controllerAs: 'freqStatCtrl'
             };
