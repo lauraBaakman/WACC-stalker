14dffe941c59beef776f7a47723ed45562327013
Laura Baakman <laurabaakman@gmail.com>
2014-10-24 11:40:59 +0200

    We have a start stalking button and an error, adding the login options is next.

--- www/frontend/app/scripts/controllers/login-controller.js
+++ www/frontend/app/scripts/controllers/login-controller.js
@@ -3,6 +3,7 @@ define(['./module'], function(controllers) {
     controllers.controller('LoginCtrl', ['$scope', 'stalkerService',
 
         function($scope, stalkerService) {
+            $scope.controllername = "LoginCtrl"
             $scope.error = "";
             var controller = this;      
 
@@ -22,20 +23,24 @@ define(['./module'], function(controllers) {
             ];
 
             this.startStalking = function(){
-                stalkerService.commitStalker();
                 if(stalkerService.isLoggedIn()){
+                    console.log('stalkerService.isLoggedIn returned true');
                     $scope.stalking = true;
+                    stalkerService.commitStalker();
                 } else {
+                    console.log('stalkerService.isLoggedIn returned false');
                     $scope.error = " You need to be logged on to at least one social network.";
                 }
             };
 
             $scope.$on('loggedInEvent', function (event, data) {
+                console.log(data);
                 $scope.error = "";
                 directives.social[data].loggedIn = true;
             });
 
             $scope.$on('loggedOutEvent', function (event, data) {
+                console.log(data);
                 $scope.error = " You need to be logged on to at least one social network.";
                 directives.social[data].loggedIn = false;
             });            
