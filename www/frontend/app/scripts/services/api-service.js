e08b759bac63546c2db50a788f812a985c0cf192
Laura Baakman <laurabaakman@gmail.com>
2014-11-02 15:10:13 +0100

    Changed the port in the api service to match the default port of the backend.|

--- www/frontend/app/scripts/services/api-service.js
+++ www/frontend/app/scripts/services/api-service.js
@@ -4,7 +4,7 @@ define(['./module'], function(services) {
     services.service('apiService', ['$http',
         function($http) {
             return {
-                baseurl: 'http://127.0.0.1:5000/',
+                baseurl: 'http://127.0.0.1:8000/',
 
                 // GETS
                 getAllSearches: function() {
